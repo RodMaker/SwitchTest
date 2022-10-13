722be6fbf154e38752a5945157bac9c0202ba09c
RodMaker <83619630+RodMaker@users.noreply.github.com>
2022-10-13 10:54:40 +0100

    initial commit

---
+++ Assets/SwitchTest.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int a, b;
+        a = 10;
+        b = 12;
+        if (a<b)
+        {
+            Debug.Log("a is less than b");
+        }
+        else
+        {
+            Debug.Log("a isn't less than b");
+            Debug.Log("a still isn't less than b");
+        }
+
+        int testvalue = 1;
+        switch (testvalue)
+        {
+            case 1:
+                Debug.Log("testvalue is 1");
+                break;
+            case 2:
+                Debug.Log("testvalue is 2");
+                break;
+            default:
+                Debug.Log("testvalue default");
+                break;
+        }
+
+        Debug.Log("End of start method");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
